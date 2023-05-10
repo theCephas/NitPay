6db5b2148e8c94b036977bd1270baec4638c6579
theCephas <oshoiseoluwa@gmail.com>
2023-05-10 13:03:16 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$('.close-btn').on('click', function() {
+        $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
+        $('main').css('display', 'block');
+});
+
+$('#deposit').on('click', function(){
+        $('#deposit-section').css('display', 'block');
+        $('main').css('display', 'none');
+});
+
+// $('#close-btn').on('click', function() {
+//         $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
+//         $('main').css('display', 'block');
+// });
+
+$('#withdraw').on('click', function(){
+        $('#withdraw-section').css('display', 'block');
+        $('main').css('display', 'none');
+});
+
+// $('#close-btn').on('click', function() {
+//         $('section').css('display', 'none');
+//         $('main').css('display', 'block');
+// });
+
+$('#transfer').on('click', function(){
+        $('#transfer-section').css('display', 'block');
+        $('main').css('display', 'none');
+});
---
+++ script.ts
@@ -0,0 +1,7 @@
+// const hideBtn = document.querySelector('main');
+// const content = document.querySelector('section');
+
+// hideBtn.addEventListener('click', function () {
+//         content.classList.toggle('hide');
+// });
+
