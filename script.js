36e2239c3243c6c42122b4414575eca7f08b5861
theCephas <oshoiseoluwa@gmail.com>
2023-05-11 14:59:40 +0100

    Installed Jquery in Typescript

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-$('.close-btn').on('click', function() {
-        $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
-        $('main').css('display', 'block');
+$('.close-btn').on('click', function () {
+    $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
+    $('main').css('display', 'block');
 });
-
-$('#deposit').on('click', function(){
-        $('#deposit-section').css('display', 'block');
-        $('main').css('display', 'none');
+$('#deposit').on('click', function () {
+    $('#deposit-section').css('display', 'block');
+    $('main').css('display', 'none');
 });
-
 // $('#close-btn').on('click', function() {
 //         $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
 //         $('main').css('display', 'block');
 // });
-
-$('#withdraw').on('click', function(){
-        $('#withdraw-section').css('display', 'block');
-        $('main').css('display', 'none');
+$('#withdraw').on('click', function () {
+    $('#withdraw-section').css('display', 'block');
+    $('main').css('display', 'none');
 });
-
 // $('#close-btn').on('click', function() {
 //         $('section').css('display', 'none');
 //         $('main').css('display', 'block');
 // });
-
-$('#transfer').on('click', function(){
-        $('#transfer-section').css('display', 'block');
-        $('main').css('display', 'none');
+$('#transfer').on('click', function () {
+    $('#transfer-section').css('display', 'block');
+    $('main').css('display', 'none');
 });
