0c1d89d346d7af57e51d993f2a27721496b7e13d
theCephas <oshoiseoluwa@gmail.com>
2023-05-12 18:32:50 +0100

    New Update

--- script.js
+++ script.js
@@ -16,3 +16,57 @@ $('#transfer').on('click', function () {
     $('#transfer-section').css('display', 'block');
     $('main').css('display', 'none');
 });
+
+$('#deposit-btn').on('click', function(){
+    var accountName = $('#account-name').val();
+    
+    var accountBalance = parseInt($('#account-balance').val());
+
+    var existingbalance = parseInt($('#act-bal').text());
+
+    var newBalance = accountBalance + existingbalance;
+
+    var accountNumber = $('#account-number').val();
+
+    
+    if (accountName){
+        $('#act-name').text(accountName);
+    }
+    if (accountNumber){
+        $('#act-num').text(accountNumber);
+    }
+    
+    if(accountBalance){
+        $('#act-bal').text(newBalance);
+    }
+
+    $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
+    $('main').css('display', 'block');
+})
+
+$('#withdraw-btn').on('click', function(){
+    var accountName = $('.account-name').val();
+    
+    var accountBalance = parseInt($('.amount').val());
+
+    var existingbalance = parseInt($('#act-bal').text());
+
+    var newBalance = existingbalance - accountBalance;
+
+    var accountNumber = $('#account-number').val();
+
+    
+    if (accountName){
+        $('#act-name').text(accountName);
+    }
+    if (accountNumber){
+        $('#act-num').text(accountNumber);
+    }
+    
+    if(accountBalance){
+        $('#act-bal').text(newBalance);
+    }
+
+    $('#deposit-section, #withdraw-section, #transfer-section').css('display', 'none');
+    $('main').css('display', 'block');
+})
